1a85cc3f53d0c7cf0cef6ec6cb845f7c96514e22
DanielFnevco <daniel.fisher@nevadacountyca.gov>
2025-08-22 16:52:20 -0700

    Update Disable-SearchHistory.ps1

--- Disable-SearchHistory.ps1
+++ Disable-SearchHistory.ps1
@@ -1,7 +1,3 @@
 # Disable device search history
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" `
     -Name "IsDeviceSearchHistoryEnabled" -Value 0 -Type DWord
-
-# Enable device search history
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" `
-    -Name "IsDeviceSearchHistoryEnabled" -Value 1 -Type DWord
