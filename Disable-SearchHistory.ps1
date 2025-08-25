d6e8571820ba8b9c0101231e6bbb453ee0b5da7c
DanielFnevco <daniel.fisher@nevadacountyca.gov>
2025-08-25 13:25:49 -0700

    Update Disable-SearchHistory.ps1

--- Disable-SearchHistory.ps1
+++ Disable-SearchHistory.ps1
@@ -1,3 +1,23 @@
-# Disable device search history
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings" `
-    -Name "IsDeviceSearchHistoryEnabled" -Value 0 -Type DWord
+# Disable device search history and log changes
+$LogFile = "$env:USERPROFILE\Documents\SearchHistoryDisable.log"
+
+# Function to write logs
+function Write-Log {
+    param([string]$Message)
+    $Timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    Add-Content -Path $LogFile -Value "$Timestamp - $Message"
+}
+
+try {
+    # Registry path
+    $RegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings"
+    $RegName = "IsDeviceSearchHistoryEnabled"
+
+    # Disable device search history
+    Set-ItemProperty -Path $RegPath -Name $RegName -Value 0 -Type DWord
+
+    Write-Log "Successfully set $RegName to 0 at $RegPath"
+}
+catch {
+    Write-Log "ERROR: Failed to update registry. $_"
+}
